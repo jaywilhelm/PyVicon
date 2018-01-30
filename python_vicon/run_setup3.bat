ac5d9d1eb48cf002d1122bf5d937085f459993c7
JW <jwilhelm@ohio.edu>
2018-01-30 14:04:36 -0500

    Setup3 bat works for dir names with spaces

--- python_vicon/run_setup3.bat
+++ python_vicon/run_setup3.bat
@@ -9,9 +9,9 @@ set MSSDK=1
 set DISTUTILS_USE_SDK=1
 
 REM C:\WinPython-64bit-3.6.2.0Qt5\python-3.6.2.amd64\python.exe setup.py build
-%USERPROFILE%\Miniconda3\python.exe setup.py build
+"%USERPROFILE%\Miniconda3\python.exe" setup.py build
 if errorlevel 0 (
-    copy C:\Users\Jay\Desktop\PyVicon\python_vicon\build\lib.win32-3.6\pyvicon.cp36-win32.pyd .\pyvicon.pyd
+    copy build\lib.win32-3.6\pyvicon.cp36-win32.pyd .\pyvicon.pyd
 )
 echo %errorlevel%
 pause
